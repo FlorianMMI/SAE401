3c53b673430e190b660f752d2b51d80c8476e24e
FlorianMMI <florian.bounissou@etu.unilim.fr>
2025-04-10 09:15:15 +0200

    opti

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/~bounissou1/SAE401/CycleC/",
+  base: "/~bounissou1/SAE401/CycleC",
   plugins: [react(), tailwindcss()],
   preview: {
     port: 5173,
