a63709d03db27165a2669b6e4ca19f883fc995ff
FlorianMMI <florian.bounissou@etu.unilim.fr>
2025-04-10 22:47:38 +0200

    remove unused admin role check from NavBar and UserController

--- frontend/src/Component/NavBar/index.tsx
+++ frontend/src/Component/NavBar/index.tsx
@@ -3,49 +3,20 @@ import { Link } from 'react-router-dom';
 
 
 
-export async function isAdmin(){
-    const token = localStorage.getItem('token');
-    if (token == null) {
-        return false;
-    }
-    try {
-        const response = await fetch(import.meta.env.VITE_URL + `/api/getrole`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-        if (response.ok) {
-            await response.json();
-            return true;
-        } else {
-            console.error('Erreur lors de la récupération du rôle:', response.status);
-            return false;
-        }
-    } catch (error) {
-        console.error('Erreur lors de la récupération du rôle:', error);
-        return false;
-    }
-}
+
 
 
 export default function NavBar() {
     const [isAdminUser, setIsAdminUser] = React.useState<boolean>(false);
 
-    React.useEffect(() => {
-        async function checkAdmin() {
-            const result = await isAdmin();
-            setIsAdminUser(result);
-        }
-        checkAdmin();
-    }, []);
+    
     <>
         <nav className="sticky top-0 flex w-full items-center bg-orangepale py-3 shadow-dark-mild">
             <div className="flex w-full items-center justify-between px-2 mx-2">
             <div className="text-base">
                 <Link
                     className="text-warmrasberry hover:text-warmrasberry-hover"
-                    to={isAdminUser ? import.meta.env.VITE_URL + `/admin` : import.meta.env.VITE_URL + `/`}
+                    to={import.meta.env.VITE_URL + `/`}
                 >
                     Lofly
                 </Link>
